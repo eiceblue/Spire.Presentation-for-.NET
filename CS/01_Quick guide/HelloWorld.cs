eff9743acda2522ef4d709ee8ea479fd15a09796
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-10 15:00:35 +0800

    Update HelloWorld.cs

--- CS/01_Quick guide/HelloWorld.cs
+++ CS/01_Quick guide/HelloWorld.cs
@@ -5,8 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Spire.Presentation;
 
-namespace Spire.Presentation.Demo
+namespace HelloWorld
 {
     public partial class Form1 : Form
     {
@@ -17,29 +18,29 @@ namespace Spire.Presentation.Demo
 
         private void btnRun_Click(object sender, EventArgs e)
         {
-            //create PPT document
+            //Create a PPT document
             Presentation presentation = new Presentation();
 
-            //add new shape to PPT document
+            //Add a new shape to the PPT document
             RectangleF rec = new RectangleF(presentation.SlideSize.Size.Width / 2 - 250, 80, 500, 150);
             IAutoShape shape = presentation.Slides[0].Shapes.AppendShape(ShapeType.Rectangle, rec);
             
             shape.ShapeStyle.LineColor.Color = Color.White;
             shape.Fill.FillType = Spire.Presentation.Drawing.FillFormatType.None;
 
-            //add text to shape
+            //Add text to the shape
             shape.AppendTextFrame("Hello World!");
 
-            //set the font and fill style of text
+            //Set the font and fill style of the text
             TextRange textRange = shape.TextFrame.TextRange;
             textRange.Fill.FillType = Spire.Presentation.Drawing.FillFormatType.Solid;
-            textRange.Fill.SolidColor.Color = Color.Black;
-            textRange.FontHeight = 72;
-            textRange.LatinFont = new TextFont("Myriad Pro Light");
+            textRange.Fill.SolidColor.Color = System.Drawing.Color.CadetBlue;
+            textRange.FontHeight = 66;
+            textRange.LatinFont = new TextFont("Lucida Sans Unicode");
 
-            //save the document
-            presentation.SaveToFile("hello.pptx", FileFormat.Pptx2010);
-            System.Diagnostics.Process.Start("hello.pptx");
+            //Save the document
+            presentation.SaveToFile("HelloWorld.pptx", FileFormat.Pptx2010);
+            System.Diagnostics.Process.Start("HelloWorld.pptx");
         }
     }
 }
