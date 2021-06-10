a678c2e2a6351694f3a2eb964b85e4aeb556a145
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-10 15:00:54 +0800

    Update BordersAndShading.cs

--- CS/02_ParagraphAndText/BordersAndShading.cs
+++ CS/02_ParagraphAndText/BordersAndShading.cs
@@ -6,8 +6,9 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using Spire.Presentation.Drawing;
+using Spire.Presentation;
 
-namespace Spire.Presentation.Demo
+namespace BordersAndShading
 {
     public partial class Form1 : Form
     {
@@ -18,65 +19,34 @@ namespace Spire.Presentation.Demo
 
         private void btnRun_Click(object sender, EventArgs e)
         {
-            //create PPT document
+            //Load a PPT document
             Presentation presentation = new Presentation();
+            presentation.LoadFromFile(@"..\..\..\..\..\..\Data\BordersAndShading.pptx");
 
-            //set background Image
-            string ImageFile = @"..\..\..\..\..\..\Data\bg.png";
-            RectangleF rect = new RectangleF(0, 0, presentation.SlideSize.Size.Width, presentation.SlideSize.Size.Height);
-            presentation.Slides[0].Shapes.AppendEmbedImage(ShapeType.Rectangle, ImageFile, rect);
-            presentation.Slides[0].Shapes[0].Line.FillFormat.SolidFillColor.Color = Color.FloralWhite;
+            IAutoShape shape = (IAutoShape)presentation.Slides[0].Shapes[0];
 
-            //add title
-            RectangleF rec_title = new RectangleF(presentation.SlideSize.Size.Width / 2 - 200, 70, 400, 50);
-            IAutoShape shape_title = presentation.Slides[0].Shapes.AppendShape(ShapeType.Rectangle, rec_title);
-            shape_title.ShapeStyle.LineColor.Color = Color.White;
-            shape_title.Fill.FillType = Spire.Presentation.Drawing.FillFormatType.None;
-            TextParagraph para_title = new TextParagraph();
-            para_title.Text = "Border Shading";
-            para_title.Alignment = TextAlignmentType.Center;
-            para_title.TextRanges[0].LatinFont = new TextFont("Myriad Pro Light");
-            para_title.TextRanges[0].FontHeight = 36;
-            para_title.TextRanges[0].Fill.FillType = Spire.Presentation.Drawing.FillFormatType.Solid;
-            para_title.TextRanges[0].Fill.SolidColor.Color = Color.Black;
-            shape_title.TextFrame.Paragraphs.Append(para_title);
+            //Set line color and width of the border
+            shape.Line.FillType = FillFormatType.Solid;
+            shape.Line.Width = 3;
+            shape.Line.SolidFillColor.Color = Color.LightYellow;
 
-            //add new shape to PPT document
-            RectangleF rec = new RectangleF(presentation.SlideSize.Size.Width / 2 - 300, 155, 600, 300);
-            IAutoShape shape = presentation.Slides[0].Shapes.AppendShape(ShapeType.Rectangle, rec);
-
-            shape.ShapeStyle.LineColor.Color = Color.Green;
-
-            //set the gradient color of shape
+            //Set the gradient fill color of shape
             shape.Fill.FillType = Spire.Presentation.Drawing.FillFormatType.Gradient;
             shape.Fill.Gradient.GradientShape = Spire.Presentation.Drawing.GradientShapeType.Linear;
-            shape.Fill.Gradient.GradientStops.Append(1f, KnownColors.LightGreen);
-            shape.Fill.Gradient.GradientStops.Append(0, KnownColors.LightBlue);
-            shape.AppendTextFrame("Spire.Presentation for .NET is a professional PowerPoint compatible component that enables developers to create, read, write, modify, convert and Print PowerPoint documents from any .NET(C#, VB.NET, ASP.NET) platform. As an independent PowerPoint .NET component, Spire.Presentation for .NET doesn't need Microsoft PowerPoint installed on the machine.");
+            shape.Fill.Gradient.GradientStops.Append(1f, KnownColors.LightBlue);
+            shape.Fill.Gradient.GradientStops.Append(0, KnownColors.LightSkyBlue);
 
-            //add text to shape
-            shape.AppendTextFrame("Spire.Presentation for .NET is a professional PowerPoint compatible component that enables developers to create, read, write, modify, convert and Print PowerPoint documents from any .NET(C#, VB.NET, ASP.NET) platform. As an independent PowerPoint .NET component, Spire.Presentation for .NET doesn't need Microsoft PowerPoint installed on the machine.");
-
-            TextParagraph para = new TextParagraph();
-            para.Text = "Spire.Presentation for .NET support PPT, PPS, PPTX and PPSX presentation formats. It provides functions such as managing text, image, shapes, tables, animations, audio and video on slides. It also support exporting presentation slides to EMF, JPG, TIFF, PDF format etc.";
-            shape.TextFrame.Paragraphs.Append(para);
-
-            //set the font and fill style of text
-            foreach (TextParagraph paragraph in shape.TextFrame.Paragraphs)
-            {
-                paragraph.TextRanges[0].Fill.FillType = Spire.Presentation.Drawing.FillFormatType.Solid;
-                paragraph.TextRanges[0].Fill.SolidColor.Color = Color.Black;
-                paragraph.TextRanges[0].FontHeight = 20;
-                paragraph.TextRanges[0].LatinFont = new TextFont("Myriad Pro");
-                paragraph.Alignment = TextAlignmentType.Left;
-            }
-
-            //set spacing after
-            shape.TextFrame.Paragraphs[0].SpaceAfter = 80;
-
-            //save the document
-            presentation.SaveToFile("BordersAndShading.pptx", FileFormat.Pptx2007);
-            System.Diagnostics.Process.Start("BordersAndShading.pptx");
+            //Set the shadow for the shape
+            Spire.Presentation.Drawing.OuterShadowEffect shadow = new Spire.Presentation.Drawing.OuterShadowEffect();
+            shadow.BlurRadius = 20;
+            shadow.Direction = 30;
+            shadow.Distance = 8;
+            shadow.ColorFormat.Color = Color.LightSeaGreen;
+            shape.EffectDag.OuterShadowEffect = shadow;
+           
+            //Save the document
+            presentation.SaveToFile("BordersAndShading_result.pptx", FileFormat.Pptx2007);
+            System.Diagnostics.Process.Start("BordersAndShading_result.pptx");
         }
     }
 }
